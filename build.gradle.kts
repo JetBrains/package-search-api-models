119f882001a177525f5b8ed652ae122491e5534f
Lamberto Basti <lamberto.basti@jetbrains.com>
2023-06-06 22:53:13 +0200

    Migrated to PKGS version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,20 @@
-import com.vdurmont.semver4j.Semver
-import com.vdurmont.semver4j.Semver.SemverType
-import com.vdurmont.semver4j.Semver.SemverType.STRICT
-
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.vdurmont:semver4j:3.1.0")
-    }
-}
-
 plugins {
-    val kotlinVersion = "1.8.20"
-    kotlin("jvm") version kotlinVersion
-    kotlin("plugin.serialization") version kotlinVersion
+    alias(packageSearchCatalog.plugins.kotlin.jvm)
+    alias(packageSearchCatalog.plugins.kotlin.plugin.serialization)
     `maven-publish`
-    //id("io.gitlab.arturbosch.detekt") version "1.20.0"
-    //id("org.jmailen.kotlinter") version "3.10.0"
-    id("org.openapi.generator") version "6.0.0"
+//    alias(packageSearchCatalog.plugins.detekt)
+//    alias(packageSearchCatalog.plugins.kotlinter)
 }
 
 group = "org.jetbrains.packagesearch"
 version = System.getenv("GITHUB_REF")?.substringAfterLast("/") ?: "2.5.0"
 
 dependencies {
-//    detektPlugins("ch.qos.logback:logback-classic:1.2.11")
-//    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.20.0")
-    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-    api("org.jetbrains.packagesearch:package-search-version-utils")
-    api("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+//    detektPlugins(packageSearchCatalog.logback.classic)
+//    detektPlugins(packageSearchCatalog.detekt.formatting)
+    implementation(packageSearchCatalog.kotlinx.serialization.json)
+    implementation(packageSearchCatalog.kotlinx.datetime)
+    api(packageSearchCatalog.packagesearch.version.utils)
 }
 
 kotlin {
@@ -100,57 +85,5 @@ publishing {
     }
 }
 
-openApiGenerate {
-
-    file("swagger").listFiles()
-        ?.filter { it.extension.contains("yaml") }
-        ?.mapNotNull { file -> SemVer.parse(file.nameWithoutExtension)?.let { semver -> semver to file } }
-        ?.maxByOrNull { it.first }
-        ?.let { inputSpec.set(it.second.absolutePath) }
-
-    outputDir.set("$buildDir/generates/src/kotlin")
-    ignoreFileOverride.set("$projectDir/.openapi-generator-ignore")
-
-    generatorName.set("kotlin")
-    library.set("multiplatform")
-    generateApiDocumentation.set(false)
-    generateApiTests.set(false)
-    generateModelDocumentation.set(false)
-    generateModelTests.set(false)
-
-    packageName.set("org.jetbrains.packagesearch")
-    apiPackage.set("org.jetbrains.packagesearch.api")
-    invokerPackage.set("org.jetbrains.packagesearch.invoker")
-    modelPackage.set("org.jetbrains.packagesearch.model")
-
-    configOptions.set(
-        mapOf(
-            "dateLibrary" to "java8",
-            "enumPropertyNaming" to "UPPERCASE",
-            "collectionType" to "list"
-        )
-    )
-}
-
-//TODO: need to refactoring this idea when we start working on v3 API
-// See more details here: https://youtrack.jetbrains.com/issue/PKGS-695#focus=Comments-27-4906671.0-0
-tasks {
-    openApiGenerate.configure {
-        outputs.dir(outputDir)
-    }
-    register<Sync>("copyOpenApiGeneratedModels") {
-        dependsOn(openApiGenerate)
-        from(openApiGenerate) {
-            include("**/model/**")
-            includeEmptyDirs = false
-        }
-        into("$buildDir/generated/apiModels")
-    }
-}
-
 val isCi
     get() = System.getenv("CI") != null || System.getenv("CONTINUOUS_INTEGRATION") != null
-
-object SemVer {
-    fun parse(version: String, mode: SemverType = STRICT) = runCatching { Semver(version, mode) }.getOrNull()
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,11 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
+    versionCatalogs {
+        create("packageSearchCatalog") {
+            val catalogFile = file("../gradle/packagesearch.versions.toml")
+            if (catalogFile.isFile) from(files(catalogFile))
+            else from("org.jetbrains.packagesearch:packagesearch-version-catalog:1.0.0")
+        }
+    }
 }
