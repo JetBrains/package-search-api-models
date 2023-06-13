95f645ca4d681240ee466703969963509a9edc19
Lamberto Basti <basti.lamberto@gmail.com>
2023-06-13 20:45:51 +0200

    All new! Now with KMP and http client v4

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,16 @@ group = "org.jetbrains.packagesearch"
 version = "3.0.0"
 
 val GITHUB_REF: String? = System.getenv("GITHUB_REF")
+val devBranches = listOf("v3", "dev")
 version = when {
     GITHUB_REF == null -> version
     GITHUB_REF.startsWith("refs/tags/") -> GITHUB_REF.substringAfter("refs/tags/")
-    GITHUB_REF.startsWith("refs/heads/main") || GITHUB_REF.startsWith("refs/heads/dev") ->
+    GITHUB_REF.startsWith("refs/heads") && devBranches.any { it in GITHUB_REF } ->
         "$version-SNAPSHOT"
     else -> version
 }
+
+GITHUB_REF?.let { logger.lifecycle("GITHUB_REF: $it") }
 
 kotlin {
     jvm()
