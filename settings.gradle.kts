bcb5c3c5af43a914ea662e0613468d4a2c4bb470
Lamberto Basti <basti.lamberto@gmail.com>
2023-06-15 14:00:01 +0200

    added build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 @file:Suppress("UnstableApiUsage")
 
+plugins {
+    `gradle-enterprise`
+}
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
@@ -12,3 +15,15 @@ dependencyResolutionManagement {
 }
 
 include(":client")
+
+val isCi
+    get() = System.getenv("CI") == "true"
+
+gradleEnterprise {
+    buildScan {
+        server = "https://ge.labs.jb.gg/"
+        accessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY")
+            ?: extra.properties["gradleEnterpriseAccessKey"]?.toString()
+        publishAlwaysIf(isCi)
+    }
+}
