2b2849f88f59972f91e04ee66c31bdf1f4154b52
Oleg Smirnov <oleg.smirnov@jetbrains.com>
2023-09-02 17:49:22 +0200

    Add `urlHash` field to `ApiScm` model (needed for ES support)

--- src/commonMain/kotlin/org/jetbrains/packagesearch/api/v3/ApiScm.kt
+++ src/commonMain/kotlin/org/jetbrains/packagesearch/api/v3/ApiScm.kt
@@ -1,23 +1,34 @@
 package org.jetbrains.packagesearch.api.v3
 
+import korlibs.crypto.SHA256
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 public sealed interface ApiScm {
     public val url: String
+    public val urlHash: String // for ElasticSearch, can't use URL as doc ID via http call
     public val description: String?
     public val ossHealthIndex: Double?
     public val readmeUrl: String?
+
+    public companion object {
+        public fun hashScmUrl(url: String): String =
+            SHA256.create()
+                .update(url.encodeToByteArray())
+                .digest()
+                .hex
+    }
 }
 
 @Serializable
 @SerialName("github")
 public data class GitHub(
     public override val url: String,
+    public override val urlHash: String,
     public override val description: String? = null,
     public override val readmeUrl: String? = null,
-    public val httpUrl: String,
+    public val htmlUrl: String,
     public val isFork: Boolean? = null,
     public override val ossHealthIndex: Double?,
     public val stars: Int? = null,
