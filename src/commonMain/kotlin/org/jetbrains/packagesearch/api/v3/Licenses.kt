9281eec0fb036d302ef69e4a7db8473db62e3786
Nikita Vlaev <nikita.vlaev@jetbrains.com>
2023-10-23 14:28:45 +0200

    Made license url nullable

--- src/commonMain/kotlin/org/jetbrains/packagesearch/api/v3/Licenses.kt
+++ src/commonMain/kotlin/org/jetbrains/packagesearch/api/v3/Licenses.kt
@@ -11,19 +11,19 @@ public data class Licenses<T: LicenseFile>(
 @Serializable
 public sealed interface LicenseFile {
     public val name: String?
-    public val url: String
+    public val url: String?
 }
 
 @Serializable
 public data class PomLicenseFile(
     public override val name: String? = null,
-    public override val url: String,
+    public override val url: String? = null,
 ) : LicenseFile
 
 @Serializable
 public data class ScmLicenseFile(
     public override val name: String? = null,
-    public override val url: String,
+    public override val url: String? = null,
     public val htmlUrl: String? = null,
     public val spdxId: String? = null,
     public val key: String? = null,
