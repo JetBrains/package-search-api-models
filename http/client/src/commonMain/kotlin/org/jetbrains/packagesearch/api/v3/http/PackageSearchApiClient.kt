b79acd98e81551cf1f02192dbe784ed9c68217d0
Jakub Senohrabek <jakub.senohrabek@jetbrains.com>
2023-12-20 10:50:38 +0100

    Changed requests for GET methods with body to POST method

--- http/client/src/commonMain/kotlin/org/jetbrains/packagesearch/api/v3/http/PackageSearchApiClient.kt
+++ http/client/src/commonMain/kotlin/org/jetbrains/packagesearch/api/v3/http/PackageSearchApiClient.kt
@@ -11,9 +11,6 @@ import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.serialization.kotlinx.protobuf.*
 import io.ktor.util.*
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
-import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -23,6 +20,9 @@ import org.jetbrains.packagesearch.api.v3.ApiPackage
 import org.jetbrains.packagesearch.api.v3.ApiProject
 import org.jetbrains.packagesearch.api.v3.ApiRepository
 import org.jetbrains.packagesearch.api.v3.search.*
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 public expect val DefaultEngine: HttpClientEngineFactory<HttpClientEngineConfig>
 
@@ -89,50 +89,54 @@ public class PackageSearchApiClient(
     }
 
     private suspend inline fun <reified T> defaultRawRequest(
+        method: HttpMethod,
         url: Url,
         body: T,
         cache: Boolean = true,
-    ) = httpClient.get(url) {
+    ) = httpClient.request(url) {
+        this@request.method = method
         setBody(body)
         header(HttpHeaders.ContentType, ContentType.Application.Json)
         attributes.put(Attributes.Cache, cache)
     }
 
-    private suspend inline fun <reified T, reified R> defaultRequest(url: Url, body: T) =
-        defaultRawRequest<T>(url, body).body<R>()
+    private suspend inline fun <reified T, reified R> defaultRequest(method: HttpMethod, url: Url, body: T) =
+        defaultRawRequest<T>(method, url, body).body<R>()
 
-    private suspend inline fun <reified R> defaultRequest(url: Url) =
-        httpClient.get(url) {
+    private suspend inline fun <reified R> defaultRequest(method: HttpMethod, url: Url) =
+        httpClient.request(url) {
+            this@request.method = method
             header(HttpHeaders.Accept, ContentType.Application.Json)
         }.body<R>()
 
     override suspend fun getKnownRepositories(): List<ApiRepository> =
-        defaultRequest(endpoints.knownRepositories)
+        defaultRequest(HttpMethod.Get, endpoints.knownRepositories)
 
     override suspend fun getPackageInfoByIds(ids: Set<String>): Map<String, ApiPackage> =
-        defaultRequest<_, List<ApiPackage>>(endpoints.packageInfoByIds, GetPackageInfoRequest(ids))
+        defaultRequest<_, List<ApiPackage>>(HttpMethod.Post, endpoints.packageInfoByIds, GetPackageInfoRequest(ids))
             .associateBy { it.id }
 
     override suspend fun getPackageInfoByIdHashes(ids: Set<String>): Map<String, ApiPackage> =
-        defaultRequest<_, List<ApiPackage>>(endpoints.packageInfoByIdHashes, GetPackageInfoRequest(ids))
+        defaultRequest<_, List<ApiPackage>>(HttpMethod.Post, endpoints.packageInfoByIdHashes, GetPackageInfoRequest(ids))
             .associateBy { it.id }
 
     override suspend fun searchPackages(request: SearchPackagesRequest): List<ApiPackage> =
-        defaultRequest<_, List<ApiPackage>>(endpoints.searchPackages, request)
+        defaultRequest<_, List<ApiPackage>>(HttpMethod.Post, endpoints.searchPackages, request)
 
     override suspend fun startScroll(request: SearchPackagesStartScrollRequest): SearchPackagesScrollResponse =
-        defaultRequest<_, SearchPackagesScrollResponse>(endpoints.startScroll, request)
+        defaultRequest<_, SearchPackagesScrollResponse>(HttpMethod.Post, endpoints.startScroll, request)
 
     override suspend fun nextScroll(request: SearchPackagesNextScrollRequest): SearchPackagesScrollResponse =
-        defaultRequest<_, SearchPackagesScrollResponse>(endpoints.nextScroll, request)
+        defaultRequest<_, SearchPackagesScrollResponse>(HttpMethod.Post,endpoints.nextScroll, request)
 
     override suspend fun searchProjects(request: SearchProjectRequest): List<ApiProject> =
-        defaultRequest<_, List<ApiProject>>(endpoints.searchPackages, request)
+        defaultRequest<_, List<ApiProject>>(HttpMethod.Post,endpoints.searchPackages, request)
 
     override fun isOnlineFlow(pollingInterval: Duration): Flow<Boolean> = flow {
         while (true) {
             val body = GetPackageInfoRequest(setOf(ApiPackage.hashPackageId("maven:io.ktor:ktor-client-core")))
             val request = defaultRawRequest(
+                method = HttpMethod.Post,
                 url = endpoints.packageInfoByIdHashes,
                 body = body,
                 cache = false
