3f1e3b264b02e1586440ecae8a14e90ac4476948
Lamberto Basti <basti.lamberto@gmail.com>
2024-02-27 10:43:57 +0100

    Remove unused function and simplify retry condition
    Removed the unused function 'retryIfNot' from Utils.kt and simplified the retry condition set in the PackageSearchApiClient.kt. The code modification will bring less complexity and make the retry logic easier to understand.

---
+++ http/client/src/commonMain/kotlin/org/jetbrains/packagesearch/api/v3/http/PackageSearchApiClient.kt
@@ -0,0 +1,175 @@
+package org.jetbrains.packagesearch.api.v3.http
+
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.call.body
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.compression.ContentEncoding
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.request
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.Url
+import io.ktor.http.isSuccess
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.serialization.kotlinx.protobuf.protobuf
+import io.ktor.util.AttributeKey
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.protobuf.ProtoBuf
+import org.jetbrains.packagesearch.api.v3.ApiPackage
+import org.jetbrains.packagesearch.api.v3.ApiProject
+import org.jetbrains.packagesearch.api.v3.ApiRepository
+
+public class PackageSearchApiClient(
+    public val endpoints: PackageSearchEndpoints,
+    private val httpClient: HttpClient = defaultHttpClient(),
+) : PackageSearchApi {
+
+    @Suppress("UNUSED_PARAMETER")
+    @Deprecated(
+        message = "Use new constructor",
+        replaceWith = ReplaceWith("PackageSearchApiClient(endpoints, httpClient)")
+    )
+    public constructor(
+        endpoints: PackageSearchEndpoints,
+        httpClient: HttpClient = defaultHttpClient(),
+        scope: CoroutineScope,
+        pollingInterval: Duration = 1.seconds,
+    ) : this(endpoints, httpClient)
+
+    public companion object {
+
+        private fun HttpClientConfig<*>.defaultEngineConfig(protobuf: Boolean = true) {
+            install(ContentNegotiation) {
+                if (protobuf) protobuf(ProtoBuf { encodeDefaults = false })
+                json()
+            }
+            install(ContentEncoding) {
+                gzip()
+            }
+            install(HttpRequestRetry) {
+                maxRetries = 3
+                exponentialDelay()
+                retryIf(3) { _, httpResponse ->
+                    httpResponse.status.value in 500..599 || httpResponse.status == HttpStatusCode.RequestTimeout
+                }
+            }
+            install(HttpTimeout) {
+                requestTimeout = 30.seconds
+            }
+        }
+
+        public fun defaultHttpClient(
+            protobuf: Boolean = true,
+            additionalConfig: HttpClientConfig<*>.() -> Unit = {},
+        ): HttpClient = HttpClient {
+            defaultEngineConfig(protobuf)
+            additionalConfig()
+        }
+
+        public fun <T : HttpClientEngineConfig> defaultHttpClient(
+            engine: HttpClientEngineFactory<T>,
+            protobuf: Boolean = true,
+            additionalConfig: HttpClientConfig<T>.() -> Unit = {},
+        ): HttpClient = HttpClient(engine) {
+            defaultEngineConfig(protobuf)
+            additionalConfig()
+        }
+
+        public fun defaultHttpClient(
+            engine: HttpClientEngine,
+            protobuf: Boolean = true,
+            additionalConfig: HttpClientConfig<*>.() -> Unit = {},
+        ): HttpClient = HttpClient(engine) {
+            defaultEngineConfig(protobuf)
+            additionalConfig()
+        }
+
+    }
+
+    public object Attributes {
+        public val Cache: AttributeKey<Boolean> = AttributeKey("cache")
+    }
+
+    private suspend inline fun <reified T> defaultRawRequest(
+        method: HttpMethod,
+        url: Url,
+        body: T,
+        cache: Boolean = true,
+    ) = httpClient.request(url) {
+        this@request.method = method
+        setBody(body)
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+        attributes.put(Attributes.Cache, cache)
+    }
+
+    private suspend inline fun <reified T, reified R> defaultRequest(method: HttpMethod, url: Url, body: T) =
+        defaultRawRequest<T>(method, url, body).body<R>()
+
+    private suspend inline fun <reified R> defaultRequest(method: HttpMethod, url: Url) =
+        httpClient.request(url) {
+            this@request.method = method
+            header(HttpHeaders.Accept, ContentType.Application.Json)
+        }.body<R>()
+
+    override suspend fun getKnownRepositories(): List<ApiRepository> =
+        defaultRequest(HttpMethod.Get, endpoints.knownRepositories)
+
+    override suspend fun getPackageInfoByIds(ids: Set<String>): Map<String, ApiPackage> =
+        defaultRequest<_, List<ApiPackage>>(HttpMethod.Post, endpoints.packageInfoByIds, GetPackageInfoRequest(ids))
+            .associateBy { it.id }
+
+    override suspend fun getPackageInfoByIdHashes(ids: Set<String>): Map<String, ApiPackage> =
+        defaultRequest<_, List<ApiPackage>>(
+            HttpMethod.Post,
+            endpoints.packageInfoByIdHashes,
+            GetPackageInfoRequest(ids)
+        )
+            .associateBy { it.id }
+
+    override suspend fun searchPackages(request: SearchPackagesRequest): List<ApiPackage> =
+        defaultRequest<_, List<ApiPackage>>(HttpMethod.Post, endpoints.searchPackages, request)
+
+    override suspend fun startScroll(request: SearchPackagesStartScrollRequest): SearchPackagesScrollResponse =
+        defaultRequest<_, SearchPackagesScrollResponse>(HttpMethod.Post, endpoints.startScroll, request)
+
+    override suspend fun nextScroll(request: SearchPackagesNextScrollRequest): SearchPackagesScrollResponse =
+        defaultRequest<_, SearchPackagesScrollResponse>(HttpMethod.Post, endpoints.nextScroll, request)
+
+    override suspend fun searchProjects(request: SearchProjectRequest): List<ApiProject> =
+        defaultRequest<_, List<ApiProject>>(HttpMethod.Post, endpoints.searchPackages, request)
+
+    override fun isOnlineFlow(pollingInterval: Duration): Flow<Boolean> = flow {
+        while (true) {
+            val body = GetPackageInfoRequest(setOf(ApiPackage.hashPackageId("maven:io.ktor:ktor-client-core")))
+            val request = kotlin.runCatching {
+                defaultRawRequest(
+                    method = HttpMethod.Post,
+                    url = endpoints.packageInfoByIdHashes,
+                    body = body,
+                    cache = false
+                )
+            }
+            val isOnline = request
+                .map { it.status.isSuccess() }
+                .getOrDefault(false)
+            emit(isOnline)
+            delay(pollingInterval)
+        }
+    }
+
+}
+
+
---
+++ http/client/src/commonMain/kotlin/org/jetbrains/packagesearch/api/v3/http/Utils.kt
@@ -0,0 +1,43 @@
+package org.jetbrains.packagesearch.api.v3.http
+
+import io.ktor.client.plugins.*
+import io.ktor.client.request.HttpRequest
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.*
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+import org.jetbrains.packagesearch.api.v3.ApiPackage
+import org.jetbrains.packagesearch.api.v3.search.NextScrollParametersBuilder
+import org.jetbrains.packagesearch.api.v3.search.SearchParametersBuilder
+import org.jetbrains.packagesearch.api.v3.search.StartScrollParametersBuilder
+import org.jetbrains.packagesearch.api.v3.search.buildNextScrollParameters
+import org.jetbrains.packagesearch.api.v3.search.buildSearchParameters
+import org.jetbrains.packagesearch.api.v3.search.buildStartScrollParameters
+
+public fun buildUrl(action: URLBuilder.() -> Unit): Url = URLBuilder().apply(action).build()
+
+internal fun HttpMessageBuilder.header(key: String, vararg values: Any?) {
+    headers.append(key, values.joinToString())
+}
+
+internal var HttpTimeout.HttpTimeoutCapabilityConfiguration.requestTimeout: Duration?
+    get() = requestTimeoutMillis?.milliseconds
+    set(value) { requestTimeoutMillis = value?.inWholeMilliseconds }
+
+internal fun HttpRequestRetry.Configuration.constantDelay(
+    delay: Duration = 1.seconds,
+    randomization: Duration = 1.seconds,
+    respectRetryAfterHeader: Boolean = true
+) {
+    constantDelay(delay.inWholeMilliseconds, randomization.inWholeMilliseconds, respectRetryAfterHeader)
+}
+
+public suspend fun PackageSearchApiClient.searchPackages(builder: SearchParametersBuilder.() -> Unit): List<ApiPackage> =
+    searchPackages(buildSearchParameters(builder))
+
+public suspend fun PackageSearchApiClient.startScroll(builder: StartScrollParametersBuilder.() -> Unit): SearchPackagesScrollResponse =
+    startScroll(buildStartScrollParameters(builder))
+
+public suspend fun PackageSearchApiClient.nextScroll(builder: NextScrollParametersBuilder.() -> Unit): SearchPackagesScrollResponse =
+    nextScroll(buildNextScrollParameters(builder))
